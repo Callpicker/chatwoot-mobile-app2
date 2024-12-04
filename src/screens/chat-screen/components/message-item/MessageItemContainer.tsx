8cb1c9edcd65508d74f4e8363ae2331986859591
Muhsin Keloth <muhsinkeramam@gmail.com>
2024-12-04 14:50:19 +0530

    chore: hide reply item

--- src/screens/chat-screen/components/message-item/MessageItemContainer.tsx
+++ src/screens/chat-screen/components/message-item/MessageItemContainer.tsx
@@ -3,10 +3,11 @@ import { Message } from '@/types';
 import { useAppDispatch, useAppSelector } from '@/hooks';
 import { selectConversationById } from '@/store/conversation/conversationSelectors';
 import { useChatWindowContext } from '@/context';
-import { setQuoteMessage } from '@/store/conversation/sendMessageSlice';
+// import { setQuoteMessage } from '@/store/conversation/sendMessageSlice';
 import { conversationActions } from '@/store/conversation/conversationActions';
-import { inboxHasFeature, is360DialogWhatsAppChannel, useHaptic } from '@/utils';
-import { INBOX_FEATURES } from '@/constants';
+import { useHaptic } from '@/utils';
+// import { inboxHasFeature, is360DialogWhatsAppChannel, useHaptic } from '@/utils';
+// import { INBOX_FEATURES } from '@/constants';
 import { showToast } from '@/helpers/ToastHelper';
 import i18n from '@/i18n';
 import Clipboard from '@react-native-clipboard/clipboard';
@@ -27,9 +28,9 @@ export const MessageItemContainer = (props: MessageItemContainerProps) => {
   const hapticSelection = useHaptic();
   const conversation = useAppSelector(state => selectConversationById(state, conversationId));
 
-  const handleQuoteReplyAttachment = () => {
-    dispatch(setQuoteMessage(props.item as Message));
-  };
+  // const handleQuoteReplyAttachment = () => {
+  //   dispatch(setQuoteMessage(props.item as Message));
+  // };
 
   const handleCopyMessage = (content: string) => {
     hapticSelection?.();
@@ -43,20 +44,21 @@ export const MessageItemContainer = (props: MessageItemContainerProps) => {
     dispatch(conversationActions.deleteMessage({ conversationId, messageId }));
   };
 
-  const inboxSupportsReplyTo = (channel: string) => {
-    const incoming = inboxHasFeature(INBOX_FEATURES.REPLY_TO, channel);
-    const outgoing =
-      inboxHasFeature(INBOX_FEATURES.REPLY_TO_OUTGOING, channel) &&
-      !is360DialogWhatsAppChannel(channel);
+  // const inboxSupportsReplyTo = (channel: string) => {
+  //   const incoming = inboxHasFeature(INBOX_FEATURES.REPLY_TO, channel);
+  //   const outgoing =
+  //     inboxHasFeature(INBOX_FEATURES.REPLY_TO_OUTGOING, channel) &&
+  //     !is360DialogWhatsAppChannel(channel);
 
-    return { incoming, outgoing };
-  };
+  //   return { incoming, outgoing };
+  // };
 
   const getMenuOptions = (message: Message): MenuOption[] => {
-    const { messageType, content, private: isPrivate, attachments } = message;
+    const { messageType, content, attachments } = message;
+    // const { private: isPrivate } = message;
     const hasText = !!content;
     const hasAttachments = !!(attachments && attachments.length > 0);
-    const channel = conversation?.meta?.channel;
+    // const channel = conversation?.meta?.channel;
 
     const isDeleted = message.contentAttributes?.deleted;
 
@@ -74,14 +76,14 @@ export const MessageItemContainer = (props: MessageItemContainerProps) => {
       });
     }
 
-    if (!isPrivate && channel && inboxSupportsReplyTo(channel).outgoing) {
-      menuOptions.push({
-        title: i18n.t('CONVERSATION.LONG_PRESS_ACTIONS.REPLY'),
-        icon: null,
-        handleOnPressMenuOption: handleQuoteReplyAttachment,
-        destructive: false,
-      });
-    }
+    // if (!isPrivate && channel && inboxSupportsReplyTo(channel).outgoing) {
+    //   menuOptions.push({
+    //     title: i18n.t('CONVERSATION.LONG_PRESS_ACTIONS.REPLY'),
+    //     icon: null,
+    //     handleOnPressMenuOption: handleQuoteReplyAttachment,
+    //     destructive: false,
+    //   });
+    // }
 
     if (hasAttachments || hasText) {
       menuOptions.push({
