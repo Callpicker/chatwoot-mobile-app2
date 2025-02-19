23cc303d96c13b325e1fee90dd916a9ba2c05a7c
Paloma Rod <placeholder.prod@outlook.com>
2025-02-19 15:38:39 -0600

    fixed gupshup 24 hours info message plus added translations for that, whatsapp and facebook

--- src/screens/chat-screen/components/reply-box/ReplyWarning.tsx
+++ src/screens/chat-screen/components/reply-box/ReplyWarning.tsx
@@ -15,61 +15,55 @@ type ReplyWarningProps = {
   conversation: Conversation;
 };
 
-export const ReplyWarning = (props: ReplyWarningProps) => {
-  const { inbox, conversation } = props;
+export const ReplyWarning = ({ inbox, conversation }: ReplyWarningProps) => {
+  const { additionalAttributes = {}, settings = {} } = inbox;
   const channel = conversation?.meta?.channel;
   const isAPIChannel = channel === INBOX_TYPES.API;
   const isAWhatsappChannel = channel === INBOX_TYPES.WHATSAPP;
+  const isGupshupAPIChannel = isAPIChannel && settings.typeApi === 'gupshup';
 
   const replyBannerMessage = () => {
-    if (isAWhatsappChannel) {
+    if (isAWhatsappChannel || isGupshupAPIChannel) {
       return i18n.t('BANNER.TWILIO_WHATSAPP_CAN_REPLY');
     }
     if (isAPIChannel) {
-      const { additionalAttributes = {} } = inbox;
-      if (additionalAttributes) {
-        const agentReplyTimeWindowMessage = additionalAttributes?.agentReplyTimeWindowMessage;
-        return agentReplyTimeWindowMessage;
-      }
-      return '';
+      return additionalAttributes?.agentReplyTimeWindowMessage ?? '';
     }
+
     return i18n.t('BANNER.CANNOT_REPLY');
   };
 
   const replyWindowLink = () => {
-    if (isAWhatsappChannel) {
+    if (isAWhatsappChannel || isGupshupAPIChannel) {
       return REPLY_POLICY.TWILIO_WHATSAPP;
     }
-    if (!isAPIChannel) {
-      return REPLY_POLICY.FACEBOOK;
-    }
-    return '';
+
+    return !isAPIChannel ? REPLY_POLICY.FACEBOOK : '';
   };
 
   const replyWindowLinkText = () => {
-    if (isAWhatsappChannel) {
+    if (isAWhatsappChannel || isGupshupAPIChannel) {
       return i18n.t('BANNER.24_HOURS_WINDOW');
     }
-    if (!isAPIChannel) {
-      return i18n.t('BANNER.TWILIO_WHATSAPP_24_HOURS_WINDOW');
-    }
-    return '';
+
+    return !isAPIChannel ? i18n.t('BANNER.TWILIO_WHATSAPP_24_HOURS_WINDOW') : '';
   };
 
   return (
-    <Pressable
-      style={tailwind.style('flex flex-row items-center px-4 py-3 max-h-[64px] bg-ruby-700 -z-10')}>
+    <Pressable style={tailwind.style('flex flex-row flex-wrap items-center px-4 py-3 bg-ruby-700 -z-10')}>
       <Animated.View style={tailwind.style('flex-1')}>
         <Animated.Text
           style={tailwind.style(
-            'text-sm tracking-[0.32px] leading-[15px] font-inter-420-20 text-white',
-          )}>
+            'text-sm tracking-[0.32px] leading-[18px] font-inter-420-20 text-white flex-shrink-0'
+          )}
+        >
           {`${replyBannerMessage()} `}
           <Animated.Text
             onPress={() => openURL({ URL: replyWindowLink() })}
             style={tailwind.style(
-              'text-sm tracking-[0.32px] leading-[15px] font-inter-420-20 text-white underline',
-            )}>
+              'text-sm tracking-[0.32px] leading-[18px] font-inter-420-20 text-white underline'
+            )}
+          >
             {replyWindowLinkText()}
           </Animated.Text>
         </Animated.Text>
